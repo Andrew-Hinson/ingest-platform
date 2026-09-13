735c3e28f6016eddaaaf7c38ac23433fbfb61e56
Andrew Hinson <73917662+Andrew-Hinson@users.noreply.github.com>
2026-09-12 22:15:25 -0500

    added rds replication support

--- tf/modules/rds/main.tf
+++ tf/modules/rds/main.tf
@@ -3,6 +3,17 @@ resource "aws_db_subnet_group" "this" {
   subnet_ids = var.subnet_ids
 }
 
+resource "aws_db_parameter_group" "this" {
+  name   = var.name
+  family = "postgres${split(".", var.engine_version)[0]}"
+
+  parameter {
+    name         = "rds.logical_replication"
+    value        = "1"
+    apply_method = "pending-reboot"
+  }
+}
+
 resource "aws_db_instance" "this" {
   identifier             = var.name
   engine                 = "postgres"
@@ -12,6 +23,7 @@ resource "aws_db_instance" "this" {
   username               = var.username
   password               = var.password
   db_subnet_group_name   = aws_db_subnet_group.this.name
+  parameter_group_name   = aws_db_parameter_group.this.name
   vpc_security_group_ids = var.security_groups
   skip_final_snapshot    = true
   apply_immediately      = true
